cbfbeb729351ad849161cd55676dd9b2d7dd4417
Juan Galarce <j6alarce@gmail.com>
2022-05-13 18:59:48 -0400

    Se crea el componente de Header y NewBudget

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <Header/>
     </div>
   )
 }
---
+++ src/components/NewBudget.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NewBudget = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+      <form className="formulario">
+        <div className="campo">
+          <label htmlFor="">Definir Presupuesto</label>
+          <input className="nuevo-presupuesto" type="text" placeholder="Añade tu presupuesto" />
+        </div>
+        <input type="submit" value="Añadir" />
+      </form>
+    </div>
+  );
+};
+
+export default NewBudget;
