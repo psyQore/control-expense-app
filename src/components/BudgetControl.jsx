cee4208820cf694c53eb63205f52427e9fa1dbf1
Juan Galarce <j6alarce@gmail.com>
2022-05-17 11:30:22 -0400

    Se crea una funcion para formatear budget en pesos chilenos

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,17 +1,27 @@
-
 const BudgetControl = ({ budget }) => {
+  const formatQuantity = (quantity) => {
+    return quantity.toLocaleString("es-CL", {
+      style: "currency",
+      currency: "CLP",
+    });
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
-        <div>
-            Grafica Aqui
-        </div>
-        <div className="contenido-presupuesto">
-            <p>
-                <span>Presupuesto:</span> ${budget}
-            </p>
-        </div>
+      <div>Grafica Aqui</div>
+      <div className="contenido-presupuesto">
+        <p>
+          <span>Presupuesto:</span> {formatQuantity(budget)}
+        </p>
+        <p>
+          <span>Disponible:</span> {formatQuantity(0)}
+        </p>
+        <p>
+          <span>Gastado:</span> {formatQuantity(0)}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BudgetControl
+export default BudgetControl;
