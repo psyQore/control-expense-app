1d67ba528f3508a0b642c1655698b23935953576
Juan Galarce <j6alarce@gmail.com>
2022-05-17 11:49:11 -0400

    Se crea una funcion para crear el NewExpense y un state para manejar el modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import IconNewBudget from "./img/nuevo-gasto.svg";
 function App() {
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
+  const [modal, setModal] = useState(false);
+
+  const handleNewExpense = () => {
+    setModal(true);
+  };
 
   return (
     <div>
@@ -16,9 +21,15 @@ function App() {
       />
       {isValidBudget && (
         <div className="nuevo-gasto">
-          <img src={IconNewBudget} alt="Icon New Budget" />
+          <img
+            src={IconNewBudget}
+            alt="Icon New Budget"
+            onClick={handleNewExpense}
+          />
         </div>
       )}
+
+      {modal && <p>Desde Modal</p>}
     </div>
   );
 }
