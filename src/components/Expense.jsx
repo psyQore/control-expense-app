9521f79647b578dd5e239fa67001655d1ea07f4d
Juan Galarce <j6alarce@gmail.com>
2022-05-19 22:01:50 -0400

    Se muestra los datos de expense en el componente Expense

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 const Expense = ({ expense}) => {
+
+    const { name, quantity, category, id } = expense;
+
   return (
-    <div>
-        <h2>Desde Expense</h2>
+    <div className='gasto sombra'>
+       <div className='contenedor-gasto'>
+           <div className='descripcion-gasto'>
+               <p className='categoria'>
+                   {expense.category}
+               </p>
+               <p className='nombre-gasto'>{name}</p>
+           </div>
+       </div>
     </div>
   )
-}
+} 
 
 export default Expense
