8eeefc323cbaf66e593b123d5ec61d8a68646422
Juan Galarce <j6alarce@gmail.com>
2022-05-25 10:26:34 -0400

    Se instala la libreria react-swipeable-list y se añade alÃ componente expense el efecto pero aun nop funcional

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,3 +1,11 @@
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
 import { formatQuantity, formatDate } from "../helpers";
 
 import iconoAhorro from "../img/icono_ahorro.svg";
@@ -22,21 +30,36 @@ const dictionaryIconos = {
 const Expense = ({ expense }) => {
   const { name, quantity, category, id, date } = expense;
 
+  const leadingActions = () => {
+    console.log("Editar...");
+  };
+
+  const trailingActions = () => {
+    console.log("Eliminar");
+  };
+
   return (
-    <div className="gasto sombra">
-      <div className="contenido-gasto">
-        <img src={dictionaryIconos[category]} alt="Icono" />
-        <div className="descripcion-gasto">
-          <p className="categoria">{category}</p>
-          <p className="nombre-gasto">{name}</p>
-          <p className="fecha-gasto">
-            Agregado el: {""}
-            <span>{formatDate(date)}</span>
-          </p>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions}
+        trailingActions={trailingActions}
+      >
+        <div className="gasto sombra">
+          <div className="contenido-gasto">
+            <img src={dictionaryIconos[category]} alt="Icono" />
+            <div className="descripcion-gasto">
+              <p className="categoria">{category}</p>
+              <p className="nombre-gasto">{name}</p>
+              <p className="fecha-gasto">
+                Agregado el: {""}
+                <span>{formatDate(date)}</span>
+              </p>
+            </div>
+          </div>
+          <p className="cantidad-gasto">{formatQuantity(quantity)}</p>
         </div>
-      </div>
-      <p className="cantidad-gasto">{formatQuantity(quantity)}</p>
-    </div>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 };
 
